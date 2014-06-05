38c7424cdf0165ccf4f732dedb15c7c963cb5890
GMTurbo <gabe.testa@gmail.com>
2014-06-04 23:17:09 -0600

    updates

--- index.js
+++ index.js
@@ -1,13 +1,95 @@
 var Table = require('cli-table');
 var keypress = require('keypress');
+var util = require('util');
+var exec = require('child_process').exec;
+var clipboard = require('node-clipboard');
 
-// make `process.stdin` begin emitting "keypress" events
+var torrent = require('./torrent');
+
+var torrentCLI = function(){
+  
+  this.torrents = [];
+ 
+  this.addTorrent = function(){
+    var mag = clipboard.read();
+    console.log(mag);
+  };
+  
+  var version = function() { return '0.0.1';}
+
+  var createRow = function(torrent) {
+  
+    var row = new Table({
+      head: ['name', 'size', "% dl'd", 'seeders'],
+      chars: { 'top': '═' , 'top-mid': '╤' , 'top-left': '╔' , 'top-right': '╗'
+         , 'bottom': '═' , 'bottom-mid': '╧' , 'bottom-left': '╚' , 'bottom-right': '╝'
+         , 'left': '║' , 'left-mid': '╟' , 'mid': '─' , 'mid-mid': '┼'
+         , 'right': '║' , 'right-mid': '╢' , 'middle': '│' },
+      colWidth: [30,20,20,20]
+    });
+    row.push([torrent.name,torrent.size,torrent.percentDone.toFixed(3) + "%", torrent.seeders]);
+    return row.toString();
+    
+  };
+  
+  var updateUI = function(){
+    
+    exec('clear', function(error, stdout, stderr){
+      
+          util.puts(stdout);
+          
+          var mainWindow = new Table({
+          head: ['torrentCLI ' + version() + ' (a)Add Torrent from clipboard (k)Show hotkeys']
+        , colWidths: [70],
+          chars: { 'top': '═' , 'top-mid': '╤' , 'top-left': '╔' , 'top-right': '╗'
+           , 'bottom': '═' , 'bottom-mid': '╧' , 'bottom-left': '╚' , 'bottom-right': '╝'
+           , 'left': '║' , 'left-mid': '╟' , 'mid': '─' , 'mid-mid': '┼'
+           , 'right': '║' , 'right-mid': '╢' , 'middle': '│' },
+        });
+        // table is an Array, so you can `push`, `unshift`, `splice` and friends
+        for(var i =0 ; i < this.torrents.length; i++){
+          this.torrents[i].percentDone += (Math.random() * 0.1);
+          mainWindow.push(
+               [createRow(this.torrents[i])]
+           );
+        }
+
+        console.log(mainWindow.toString());
+        
+      }.bind(this));
+      
+  }.bind(this);
+  
+  this.interval = setInterval(function(){
+    updateUI();
+  }.bind(this), 1500);
+
+};
+
+var cli = new torrentCLI();
+
+  //make `process.stdin` begin emitting "keypress" events
 keypress(process.stdin);
 
 // listen for the "keypress" event
 process.stdin.on('keypress', function (ch, key) {
   console.log('got "keypress"', key);
+  switch(key.name){
+    case 'a'://add
+      cli.addTorrent();
+      break;
+    case 'k'://show options
+      cli.showOptions();
+      break;
+  }
+  
+  
   if (key && key.ctrl && key.name == 'c') {
     process.stdin.pause();
+    clearInterval(cli.interval);
   }
+  
 });
+
+process.stdin.setRawMode(true);
+process.stdin.resume();
---
+++ torrent.js
@@ -0,0 +1,193 @@
+/*
+    Copyright (c) 2014 Bastien Clément
+
+    Permission is hereby granted, free of charge, to any person obtaining a
+    copy of this software and associated documentation files (the
+    "Software"), to deal in the Software without restriction, including
+    without limitation the rights to use, copy, modify, merge, publish,
+    distribute, sublicense, and/or sell copies of the Software, and to
+    permit persons to whom the Software is furnished to do so, subject to
+    the following conditions:
+
+    The above copyright notice and this permission notice shall be included
+    in all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+    OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+    IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+    CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+    TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+    SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+var fs = require("fs");
+var path = require("path");
+var events = require("events");
+var crypto = require("crypto");
+var mkdirp = require("mkdirp");
+var torrentStream = require("torrent-stream");
+
+var TorrentEngine = new events.EventEmitter();
+
+TorrentEngine.ready = false;
+TorrentEngine.done = false;
+TorrentEngine.opts = {
+    connections: 100,
+    uploads: 10,
+    path: process.cwd(),
+    verify: true,
+    dht: 10000,
+    tracker: true,
+    name: "tget"
+};
+TorrentEngine.total_pieces = 0;
+TorrentEngine.finished_pieces = 0;
+TorrentEngine.connect = [];
+
+var engine;
+var ephemeral = false;
+var wait = false;
+var download_snapshot = 0;
+
+function checkDone() {
+    if(TorrentEngine.finished_pieces == TorrentEngine.total_pieces) {
+        TorrentEngine.done = true;
+        TorrentEngine.emit("done");
+    }
+};
+
+TorrentEngine.load = function(torrent, opts, cb) {
+    // Missing argument
+    if(!torrent) {
+        return cb(null);
+    }
+
+    // Options
+    if(opts.c) { TorrentEngine.opts.connections = opts.c; }
+    if(opts.d) { TorrentEngine.opts.dht = (!opts.d || opts.d === true) ? false : opts.d; }
+    if(opts.t) { TorrentEngine.opts.tracker = false; }
+    if(opts.u) { TorrentEngine.opts.uploads = opts.u; }
+    if(opts.w) { wait = true; }
+
+    if(opts.e) {
+        ephemeral = true;
+        TorrentEngine.opts.path = null;  // Will download to /tmp
+    }
+
+    if(opts.p) {
+        if(Array.isArray(opts.p)) {
+            TorrentEngine.connect = opts.p;
+        } else {
+            TorrentEngine.connect.push(opts.p);
+        }
+    }
+
+    // Magnet link
+    if(torrent.slice(0, 7) == "magnet:") {
+        return cb(torrent);
+    }
+
+    // HTTP link
+    var https = torrent.slice(0, 8) == "https://";
+    if(https || torrent.slice(0, 7) == "http://") {
+        var http = require(https ? "https" : "http");
+        http.get(torrent, function(res) {
+            var buffers = [];
+
+            res.on("data", function(data) {
+                buffers.push(data);
+            })
+
+            res.on("end", function() {
+                cb(Buffer.concat(buffers));
+            })
+        });
+        return;
+    }
+
+    // Attempt to read a local file
+    return cb(fs.readFileSync(torrent));
+};
+
+TorrentEngine.init = function(torrent, opts) {
+    // TorrentStream instance
+    TorrentEngine.engine = engine = torrentStream(torrent, opts || TorrentEngine.opts);
+
+    if(opts.l) {
+        engine.listen(opts.l);
+    }
+
+    // Explicit peer connection
+    TorrentEngine.connect.forEach(function(peer) {
+        engine.connect(peer);
+    });
+
+    // Wait for torrent metadata to be available
+    engine.on("ready", function() {
+        TorrentEngine.ready = true;
+        TorrentEngine.total_pieces = engine.torrent.pieces.length;
+        TorrentEngine.torrent = engine.torrent;
+        TorrentEngine.wires = engine.swarm.wires;
+        TorrentEngine.files = engine.files.filter(function(file) {
+            // TODO: maybe a filtering option
+            return true;
+        });
+
+        // Start the download of every file (unless -w)
+        if(!wait) {
+            TorrentEngine.files.forEach(function(file) {
+                file.select();
+            });
+        }
+
+        // Resuming a download ?
+        for(var i = 0; i < TorrentEngine.total_pieces; i++) {
+            if(engine.bitfield.get(i)) {
+                ++TorrentEngine.finished_pieces;
+            }
+        }
+        checkDone();
+
+        // New piece downlaoded
+        engine.on("verify", function() {
+            download_snapshot = engine.swarm.downloaded;
+            ++TorrentEngine.finished_pieces;
+            checkDone();
+        });
+
+        // Pause or resume the swarm when interest changes
+        engine.on("uninterested", function() { engine.swarm.pause(); });
+        engine.on("interested", function() { engine.swarm.resume(); });
+
+        // We're ready
+        TorrentEngine.emit("ready");
+    });
+};
+
+TorrentEngine.downloadPercent = function() {
+    // Return range: 0-100
+    return Math.floor((TorrentEngine.finished_pieces/TorrentEngine.total_pieces) * 100);
+};
+
+TorrentEngine.downloadSpeed = function() {
+    return engine.swarm.downloadSpeed();
+};
+
+TorrentEngine.downloadedBytes = function() {
+    return (TorrentEngine.finished_pieces * engine.torrent.pieceLength) + (engine.swarm.downloaded - download_snapshot);
+};
+
+TorrentEngine.exit = function(cb) {
+    engine.destroy(function() {
+        if(ephemeral || TorrentEngine.done) {
+            engine.remove(!ephemeral, function() {
+                cb()
+            });
+        } else {
+            cb();
+        }
+    });
+};
+
+module.exports = TorrentEngine;
